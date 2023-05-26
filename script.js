fcdb1e1fab455f717eb80b6c5640dc5f3243cfce
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-05-26 17:27:06 +0100

    Update listeners function to change border colour of input when passwords do not match

--- script.js
+++ script.js
@@ -3,14 +3,33 @@ function listeners() {
     let confirmPasswordInput = document.querySelector("#passwordConfirmation")
     let confirmPasswordMsg = document.querySelector("#confirmPasswordMsg")
 
+    function changeInputColours(colour) {
+        passwordInput.style.borderBottom = `2px solid ${colour}`;
+        confirmPasswordInput.style.borderBottom = `2px solid ${colour}`;
+    }
+
     confirmPasswordInput.addEventListener("keyup" , () => {
         if (passwordInput.value !== confirmPasswordInput.value) {
             confirmPasswordMsg.innerText = "Passwords do not match."
+            changeInputColours("red")
         }
         else {
             confirmPasswordMsg.innerText = "Passwords match."
+            changeInputColours("#e25482")
+        }
+    })
+
+    passwordInput.addEventListener("keyup" , () => {
+        if (passwordInput.value !== confirmPasswordInput.value) {
+            confirmPasswordMsg.innerText = "Passwords do not match."
+            changeInputColours("red")
+        }
+        else {
+            confirmPasswordMsg.innerText = "Passwords match."
+            changeInputColours("#e25482")
         }
     })
 }
 
 listeners()
+

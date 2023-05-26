4ecec45c5a81b14aebb3699521ba22cef3272419
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-05-26 17:34:39 +0100

    Clean and update listeners function

--- script.js
+++ script.js
@@ -1,33 +1,24 @@
 function listeners() {
-    let passwordInput = document.querySelector("#password")
+    let passwordInputs = document.querySelectorAll(".passwordInput")
     let confirmPasswordInput = document.querySelector("#passwordConfirmation")
     let confirmPasswordMsg = document.querySelector("#confirmPasswordMsg")
 
     function changeInputColours(colour) {
-        passwordInput.style.borderBottom = `2px solid ${colour}`;
-        confirmPasswordInput.style.borderBottom = `2px solid ${colour}`;
+        passwordInputs[0].style.borderBottom = `2px solid ${colour}`;
+        passwordInputs[1].style.borderBottom = `2px solid ${colour}`;
     }
 
-    confirmPasswordInput.addEventListener("keyup" , () => {
-        if (passwordInput.value !== confirmPasswordInput.value) {
-            confirmPasswordMsg.innerText = "Passwords do not match."
-            changeInputColours("red")
-        }
-        else {
-            confirmPasswordMsg.innerText = "Passwords match."
-            changeInputColours("#e25482")
-        }
-    })
-
-    passwordInput.addEventListener("keyup" , () => {
-        if (passwordInput.value !== confirmPasswordInput.value) {
-            confirmPasswordMsg.innerText = "Passwords do not match."
-            changeInputColours("red")
-        }
-        else {
-            confirmPasswordMsg.innerText = "Passwords match."
-            changeInputColours("#e25482")
-        }
+    passwordInputs.forEach(pwdInput => {
+        pwdInput.addEventListener("keyup" , () => {
+            if (passwordInputs[0].value !== passwordInputs[1].value) {
+                confirmPasswordMsg.innerText = "Passwords do not match."
+                changeInputColours("red")
+            }
+            else {
+                confirmPasswordMsg.innerText = "Passwords match."
+                changeInputColours("#e25482")
+            }
+        })
     })
 }
 
